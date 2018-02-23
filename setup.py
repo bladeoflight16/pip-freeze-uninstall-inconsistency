3f02ac1cf5deb6e1c62b7bf83face2ff055ddae4
bladeoflight16 <bladeoflight16@gmail.com>
2018-02-22 20:11:21 -0500

    Adding files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='mypack',
+    version='1.0',
+    description='For demonstrating pip bug',
+    author='bladeoflight16',
+    packages=['mypack'],
+)
